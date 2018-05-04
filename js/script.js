740544e963a216e6539abd05f7e30000bfef0a69
moritzweberde <moritzweber3@yahoo.de>
2018-05-04 15:02:39 +0200

    files

--- js/script.js
+++ js/script.js
@@ -29,5 +29,13 @@ $(document).ready(function() {
    });
 
 
-
 });
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
